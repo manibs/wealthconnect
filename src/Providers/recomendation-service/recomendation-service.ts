28921cfe26aadaa4328cdfac4ce34a9e7aaa6a8c
Mani <manibhushan.sharma@gmail.com>
2018-09-29 00:04:57 +0530

    integrate with API

--- src/Providers/recomendation-service/recomendation-service.ts
+++ src/Providers/recomendation-service/recomendation-service.ts
@@ -10,7 +10,7 @@ import { HTTP } from '@ionic-native/http';
 */
 @Injectable()
 export class RecomendationServiceProvider {
-
+  public data:any;
   public Recomendations: any;
   public baseUrl = 'https://gentle-plains-98312.herokuapp.com/api/';
   constructor(public http: HTTP) {
@@ -20,15 +20,40 @@ export class RecomendationServiceProvider {
   /**
    * GetRecomendations
    */
+
+  public load()
+  {
+   if (this.data) {
+    // already loaded data
+    return Promise.resolve(this.data);
+  }
+
+  // don't have the data yet
+  return new Promise(resolve => {
+    // We're using Angular HTTP provider to request the data,
+    // then on the response, it'll map the JSON data to a parsed JS object.
+    // Next, we process the data and resolve the promise with the new data.
+    this.http.get(this.baseUrl+'Recommendations',{},{})
+    // this.http.get('https://randomuser.me/api/?results=10',{},{})
+      //.map(res => res.json())
+      .then(data => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        this.data = data.data;
+        resolve(this.data);
+      });
+  });
+}
+
   public GetRecomendations() {
     //return this.Recomendations; 
-    if (this.Recomendations) {
-      // already loaded data
-      return Promise.resolve(this.Recomendations);
-    }
+    // if (this.Recomendations) {
+    //   // already loaded data
+    //   return Promise.resolve(this.Recomendations);
+    // }
 
     // don't have the data yet
-     return new Promise(resolve => {
+      return new Promise(resolve => {
        
       // We're using Angular HTTP provider to request the data,
       // then on the response, it'll map the JSON data to a parsed JS object.
@@ -36,10 +61,10 @@ export class RecomendationServiceProvider {
       this.http.get(this.baseUrl+'Recommendations', {}, {})
         .then(data => {
           this.Recomendations = data.data;
-          console.log(this.Recomendations);
+          //console.log(this.Recomendations);
           return this.Recomendations;
         });
-    });
+     });
 
    
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,28 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {RecomendationServiceProvider} from '../../Providers/recomendation-service/recomendation-service';
+import { RecomendationServiceProvider } from '../../Providers/recomendation-service/recomendation-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-     public recomendations: any;
+  public recomendations: any;
 
   constructor(public navCtrl: NavController, public recomendationServiceProvider: RecomendationServiceProvider) {
     this.loadRecomendation();
   }
 
-  public loadRecomendation()
-  {
-    //this.recomendations='[{"_id":"5b746027f7cc850004339c4a","Stock":"TCS","CMP":"100","target":"110","stopploss":"90","RecomendedOn":"2018-08-15T00:00:00.000Z","CallType":"Stocks","__v":0},{"_id":"5b74604ff7cc850004339c4b","Stock":"crudeoil","CMP":"4000","target":"4010","stopploss":"3990","RecomendedOn":"2018-08-15T00:00:00.000Z","CallType":"Commodity","__v":0}]';
-    console.log('test');
-    this.recomendationServiceProvider.GetRecomendations()
-    .then(data=> {
-      console.log('xx');
-      console.log(data);
-     this.recomendations= data;
-    })
+  public loadRecomendation() {
+    this.recomendationServiceProvider.load()
+      .then(data => {
+        this.recomendations = JSON.parse(data);        
+      });
+
   }
 
 }
